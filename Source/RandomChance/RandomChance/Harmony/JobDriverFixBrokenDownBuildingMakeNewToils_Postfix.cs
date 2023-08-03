394a5f30eb14ef7fd1e7180ee9752c02bab95a61
Steve <stevenzybertusn@gmail.com>
2023-08-03 13:58:39 -0400

    finally, it fucking works!

--- Source/RandomChance/RandomChance/Harmony/JobDriverFixBrokenDownBuildingMakeNewToils_Postfix.cs
+++ Source/RandomChance/RandomChance/Harmony/JobDriverFixBrokenDownBuildingMakeNewToils_Postfix.cs
@@ -1,8 +1,10 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System;
 using System.Collections.Generic;
 using Verse;
 using Verse.AI;
+using Verse.Noise;
 
 namespace RandomChance
 {
@@ -15,66 +17,66 @@ namespace RandomChance
             List<Toil> newToils = new(__result);
             int numToils = newToils.Count;
 
-            Log.Message("[RC] hi");
-
             // new toil with delegate
             Toil customToil = new Toil
             {
-                tickAction = delegate
+                initAction = delegate
                 {
                     if (!__instance.pawn.IsColonyMech)
                     {
-                        float failureChance = RandomChanceSettings.ElectricalRepairFailureChance; // 5% by default
+                        float failureChance = RandomChanceSettings.ElectricalRepairFailureChance; // 10% by default
                         if (Rand.Chance(failureChance))
                         {
                             IBillGiverWithTickAction billGiver = __instance.job.GetTarget(TargetIndex.A).Thing as IBillGiverWithTickAction;
-                            int pawnsAvgSkillLevel = (int)__instance.pawn.skills.AverageOfRelevantSkillsFor(billGiver.GetWorkgiver().workType);
-                            Building building = __instance.job.GetTarget(TargetIndex.A).Thing as Building;
-
-                            SimpleCurve chanceCurve = new()
+                            if (billGiver != null)
                             {
-                                { 0, 0.5f },
-                                { 3, 0.5f },
-                                { 6, 0.3f },
-                                { 8, 0.2f },
-                                { 14, 0.1f },
-                                { 18, 0.05f },
-                                { 20, 0.02f }
-                            };
+                                int pawnsAvgSkillLevel = (int)__instance.pawn.skills.AverageOfRelevantSkillsFor(billGiver.GetWorkgiver().workType);
+                                Building building = __instance.job.GetTarget(TargetIndex.A).Thing as Building;
 
-                            if (Rand.Chance(0.5f)) // change back to evaluate the curve
-                            {
-                                if (building != null)
+                                SimpleCurve chanceCurve = new()
                                 {
-                                    IntVec3 explosionCenter = building.Position;
-                                    Map explosionMap = building.Map;
-                                    int explosionRadius = Rand.RangeInclusive(2, 5);
-
-                                    // make some filth too, go all out
-                                    GenExplosion.DoExplosion(explosionCenter, explosionMap, explosionRadius, DamageDefOf.EMP, null, Rand.RangeInclusive(1, 9));
-                                    if (Rand.Value < 0.25f)
+                                    { 0, 0.8f },
+                                    { 3, 0.7f },
+                                    { 6, 0.5f },
+                                    { 8, 0.3f },
+                                    { 14, 0.2f },
+                                    { 18, 0.2f },
+                                    { 20, 0.2f }
+                                };
+                                Log.Message("pawnsAvgSkillLevel" + pawnsAvgSkillLevel);
+                                if (Rand.Chance(chanceCurve.Evaluate(pawnsAvgSkillLevel)))
+                                {
+                                    if (building != null)
                                     {
-                                        GenExplosion.DoExplosion(explosionCenter, explosionMap, explosionRadius, DamageDefOf.Bomb, null, Rand.RangeInclusive(3, 12));
+                                        IntVec3 explosionCenter = building.Position;
+                                        Map explosionMap = building.Map;
+                                        int explosionRadius = Rand.RangeInclusive(2, 5);
+
+                                        GenExplosion.DoExplosion(explosionCenter, explosionMap, explosionRadius, DamageDefOf.EMP, null, Rand.RangeInclusive(1, 9));
+                                        if (Rand.Value < 0.25f)
+                                        {
+                                            GenExplosion.DoExplosion(explosionCenter, explosionMap, explosionRadius, DamageDefOf.Bomb, null, Rand.RangeInclusive(3, 12));
+                                            FireUtility.TryStartFireIn(building.Position, building.Map, RandomChanceSettings.FailedCookingFireSize);
+                                            MoteMaker.MakeColonistActionOverlay(__instance.pawn, ThingDefOf.Mote_ColonistFleeing);
+                                        }
+                                        Messages.Message("RC_ElectricalRepairFailure".Translate(__instance.pawn.Named("PAWN")), __instance.pawn, MessageTypeDefOf.NegativeEvent);
                                     }
-                                }
 
-                                BodyPartRecord bodyPart = __instance.pawn.RaceProps.body.GetPartsWithTag(BodyPartTagDefOf.ManipulationLimbCore).RandomElement();
+                                    BodyPartRecord bodyPart = __instance.pawn.RaceProps.body.GetPartsWithTag(BodyPartTagDefOf.ManipulationLimbCore).RandomElement();
 
-                                if (bodyPart != null)
-                                {
-                                    Thing Components = __instance.job.GetTarget(TargetIndex.B).Thing;
-                                    Components.Destroy();
+                                    if (bodyPart != null)
+                                    {
+                                        HediffDef burnHediffDef = HediffDefOf.Burn;
 
-                                    HediffDef burnHediffDef = HediffDefOf.Burn;
+                                        float severity = 0.9f; // change later
 
-                                    float severity = 0.9f; // change later
+                                        Hediff hediff = HediffMaker.MakeHediff(burnHediffDef, __instance.pawn, bodyPart);
+                                        hediff.Severity = severity;
+                                        __instance.pawn.health.AddHediff(hediff);
 
-                                    Hediff hediff = HediffMaker.MakeHediff(burnHediffDef, __instance.pawn, bodyPart);
-                                    hediff.Severity = severity;
-                                    __instance.pawn.health.AddHediff(hediff);
-
-                                    Find.TickManager.slower.SignalForceNormalSpeedShort();
-                                    __instance.pawn.stances.stunner.StunFor(120, __instance.pawn, false, false);
+                                        Find.TickManager.slower.SignalForceNormalSpeedShort();
+                                        __instance.pawn.stances.stunner.StunFor(120, __instance.pawn, false, false);
+                                    }
                                 }
                             }
                         }
